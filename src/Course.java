c50fd7eb052358e08404cd024f80c5cc865f3cfe
KevinTranbar <kt222sx@student.lnu.se>
2026-01-20 14:49:43 +0100

    object task 1

--- src/Course.java
+++ src/Course.java
@@ -1,2 +1,127 @@
 public class Course {
+
+    //Attributes
+    String CourseCode;
+    String CourseName;
+    int MaxStudents;
+    int MaxTeachers;
+    String[] Students;
+    String[] Teachers;
+    int NumOfStudents;
+    int NumOfTeachers;
+
+    //Constructors - makes entity based on code, name, max students / teachers
+    public Course() {
+        System.out.println("Course not selected");
+    }
+    public Course(String CourseCode, String CourseName, int MaxStudents, int MaxTeachers) {
+        this.CourseCode = CourseCode;
+        this.CourseName = CourseName;
+        this.MaxStudents = MaxStudents;
+        this.MaxTeachers = MaxTeachers;
+
+        this.Students = new String[MaxStudents];
+        this.Teachers = new String[MaxTeachers];
+    }
+
+    //Getters
+    public String GetCourseCode() {
+        return CourseCode;
+    }
+    public String GetCourseName() {
+        return CourseName;
+    }
+    public int GetNumOfStudents() {
+        return NumOfStudents;
+    }
+    public int GetNumOfTeachers() {
+        return NumOfTeachers;
+    }
+
+    //Setters
+    public void SetCourseCode(String NewCourseCodes) {
+        this.CourseCode = NewCourseCodes;
+    }
+    public void SetCourseName(String NewCourseName) {
+        this.CourseName = NewCourseName;
+    }
+
+    //Methods
+    public void AddStudent(String Name) {
+        if (NumOfStudents >= MaxStudents) {
+            System.out.println("Cannot add more students");
+            return;
+        }
+        for (int i = 0; i < NumOfStudents; i++) {
+            if (Students[i].equals(Name)) {
+                System.out.println(Name + " is already registered");
+                return;
+            }
+
+        }
+        Students[NumOfStudents] = Name;
+        NumOfStudents++;
+
+    }
+    public void DropStudent(String Name) {
+        for (int i = 0; i < NumOfStudents; i++) {
+            if (Students[i].equals(Name)) {
+                Students[i] = null;
+                return;
+            }
+        }
+        System.out.println("Student not found");
+
+    }
+
+    public void AddTeacher(String Name) {
+        if (NumOfTeachers >= MaxTeachers) {
+            System.out.println("Cannot add more teachers");
+            return;
+        }
+        for (int i = 0; i < NumOfTeachers; i++) {
+            if (Teachers[i].equals(Name)) {
+                System.out.println(Name + " is already registered");
+                return;
+            }
+
+        }
+        Teachers[NumOfTeachers] = Name;
+        NumOfTeachers++;
+
+    }
+    public void DropTeacher(String Name) {
+        for (int i = 0; i < NumOfTeachers; i++) {
+            if (Teachers[i].equals(Name)) {
+                Teachers[i] = null;
+                return;
+            }
+        }
+        System.out.println("Teacher not found");
+
+    }
+
+    public void DisplayAllStudents() {
+        if (NumOfStudents <= 0)
+            System.out.println("No students");
+
+        for (int i = 0; i < MaxStudents; i++) {
+            if (Students[i] != null) {
+                System.out.println(Students[i]);
+            }
+        }
+
+    }
+    public void DisplayAllTeachers() {
+        if (NumOfTeachers <= 0)
+            System.out.println("No teachers");
+
+        for (int i = 0; i < MaxTeachers; i++) {
+            if (Teachers[i] != null) {
+                System.out.println(Teachers[i]);
+            }
+        }
+
+    }
+
 }
---
+++ src/Test.java
@@ -0,0 +1,26 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        Course course = new Course("ik231", "Programing", 3, 4);
+
+        course.AddStudent("Kevin");
+        course.AddStudent("Deja");
+        course.AddStudent("Neo");
+        course.AddTeacher("Kevin");
+        course.AddTeacher("Deja");
+        course.AddTeacher("Neo");
+        course.AddTeacher("hej");
+        course.DropTeacher("hej");
+
+
+        System.out.println("Students: ");
+        course.DisplayAllStudents();
+        System.out.println("Teachers: ");
+        course.DisplayAllTeachers();
+
+
+
+    }
+
+}
